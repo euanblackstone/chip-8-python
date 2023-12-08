c0af133733e02b4d0c7ee7227b1a37324f8f251e
Euan Blackstone <blackstoneeuan@gmail.com>
2023-12-08 00:23:28 -0700

    filled out most opcodes and started emulator class

--- cpu.py
+++ cpu.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python3
+import random
+
+'''
+TODO:   opcode 0x00e0
+        opcode 0xD000
+        opcode 0xE000
+        opcode 0xF00A
+'''
+
 
 class cpu:
     bytes_in_memory = 4096
@@ -21,7 +29,18 @@ class cpu:
         self.speed_of_emulation = 10
 
     def load_rom_into_memory(self, rom_file_name):
-        print("hi")
+        try:
+            fp = open(rom_file_name, 'rb')
+            bytes_from_file = fp.read()
+            fp.close()
+        except:
+            print("Unable to open and read rom file given. Please make sure path is correct.")
+            exit(1)
+
+        for i in range(len(bytes_from_file)):
+            #dubious line ?
+            self.memory[0x200 + i] = bytes_from_file[i]
+
 
     def load_fontset_into_memory(self):
         fontset = bytearray([
@@ -48,10 +67,10 @@ class cpu:
 
     def update_timers(self):
         if self.delay_timer > 0:
-            self.delay_timer = self.delay_timer - 1 
+            self.delay_timer -= 1
         
         if self.sound_timer > 0:
-            self.sound_timer = self.sound_timer - 1
+            self.sound_timer -= 1
 
     def emulate_cycle(self):
         i = 0
@@ -69,12 +88,141 @@ class cpu:
         #play sound if including sound
 
     def execute_opcode(self, opcode):
-        x = ((opcode & 0x0f00) >> 8)
-        y = ((opcode & 0x00f0) >>4)
+        x = ((opcode & 0x0F00) >> 8)
+        y = ((opcode & 0x00F0) >>4)
 
-        match (opcode & 0xf000):
+        match (opcode & 0xF000):
             case 0x0000:
+                match (opcode):
+                    case 0x00E0:
+                        #clear the renderer
+                        print("hi")
+                    case 0x00EE:
+                        self.program_counter = self.stack.pop()
+            
+            case 0x1000:
+                self.program_counter = (opcode & 0x0FFF)
+
+            case 0x2000:
+                self.stack.append(self.program_counter)
+                self.program_counter = (opcode & 0x0FFF)
+
+            case 0x3000:
+                if self.v_registers[x] == (opcode & 0x00FF):
+                    self.program_counter += 2
+
+            case 0x4000:
+                if self.v_registers[x] != (opcode & 0x00FF):
+                    self.program_counter += 2
+
+            case 0x5000:
+                if self.v_registers[x] == self.v_registers[y]:
+                    self.program_counter += 2
+
+            case 0x6000:
+                self.v_registers[x] = (opcode & 0x00FF)
+
+            case 0x7000:
+                self.v_registers[x] = (self.v_registers[x] + (opcode & 0x00FF))
+
+            case 0x8000:
+                match (opcode & 0x000F):
+                    case 0x0:
+                        self.v_registers[x] = self.v_registers[y]
+                    case 0x1:
+                        self.v_registers[x] = (self.v_registers[x] | self.v_registers[y])
+                    case 0x2:
+                        self.v_registers[x] = (self.v_registers[x] & self.v_registers[y])
+                    case 0x3:
+                        self.v_registers[x] = (self.v_registers[x] ^ self.v_registers[y])
+                    case 0x4:
+                        sum = self.v_registers[x] + self.v_registers[y]
+
+                        self.v_registers[0xF] = 0
+
+                        if sum > 0xFF:
+                            self.v_registers[0xF] = 1
+
+                        self.v_registers[x] = (sum & 0xFF)
+                    case 0x5:
+                        self.v_registers[0xF] = 0
+
+                        if self.v_registers[x] > self.v_registers[y]:
+                            self.v_registers[0xF] = 1
+
+                        self.v_registers[x] = ((self.v_registers[x] - self.v_registers[y]) & 0xFF)
+                    case 0x6:
+                        self.v_registers[0xF] = (self.v_registers[x] & 0x1)
+                        
+                        self.v_registers[x] >>= 1
+                    case 0x7:
+                        self.v_registers[0xF] = 0
+
+                        if self.v_registers[y] > self.v_registers[x]:
+                            self.v_registers[0xF] = 1
+
+                        self.v_registers[x] = ((self.v_registers[y] - self.v_registers[x]) & 0xFF)
+                    case 0xE:
+                        self.v_registers[0xF] = (self.v_registers[x] & 0x80)
+                        self.v_registers[x] <<= 1
+
+            case 0x9000:
+                if self.v_registers[x] != self.v_registers[y]:
+                    self.program_counter += 2
+
+            case 0xA000:
+                self.i_register = (opcode & 0x0FFF)
+
+            case 0xB000:
+                self.program_counter = ((opcode & 0x0FFF) + self.v_registers[0])
+
+            case 0xC000:
+                #might need to change the end of the range
+                random_number = random.randint(1, 0xFF)
+                self.v_registers[x] = (random_number & (opcode & 0xFF))
+
+            case 0xD000:
+                #display stuff
                 print("hi")
+
+            case 0xE000:
+                match (opcode & 0x00FF):
+                    case 0x009E:
+                        #keyboard
+                        print("hi")
+                    case 0x00A1:
+                        #keyboard
+                        print("hi")
+
+            case 0xF000:
+                match (opcode & 0x00FF):
+                    case 0x0007:
+                        self.v_registers[x] = self.delay_timer
+                    case 0x000A:
+                        #pause
+                        self.is_program_paused = True
+                    case 0x0015:
+                        self.delay_timer = self.v_registers[x]
+                    case 0x0018:
+                        self.sound_timer = self.v_registers[x]
+                    case 0x001E:
+                        self.i_register += self.v_registers[x]
+                        self.i_register = (self.i_register & 0x0FFF)
+                    case 0x0029:
+                        self.i_register = (self.v_registers[x] * 5)
+                    case 0x0033:
+                        self.memory[self.i_register] = (self.v_registers[x] / 100)
+                        self.memory[self.i_register + 1] = ((self.v_registers[x] % 100) / 10)
+                        self.memory[self.i_register + 2] = (self.v_registers[x] % 10)
+                    case 0x0055:
+                        for i in range(x + 1):
+                            self.memory[self.i_register + i] = self.v_registers[i]
+                    case 0x0065:
+                        for i in range(x + 1):
+                            self.v_registers[i] = self.memory[self.i_register + i]
+
+
+                    
             case _:
                 print("Received unknown opcode %s from program. Exiting." % opcode)
                 exit(1)
---
+++ emulator.py
@@ -0,0 +1,29 @@
+import cpu
+
+'''
+TODO:   write out create_gui and start_emu_loop methods
+        actually create the screen and keypad classes and add them to constructor
+'''
+
+class emulator:
+    screen_scale = 20
+
+    def __init__(self, frequency_of_cpu, rom_file_name):
+        self.frequency_of_cpu = frequency_of_cpu
+        self.ns = 1000000000.0 / self.frequency_of_cpu
+
+        #create screen, ketboard, and cpu
+        self.cpu = cpu()
+        self.screen = None
+        self.keypad = None
+
+        self.create_gui()
+
+        self.cpu.load_fontset_into_memory()
+        self.cpu.load_rom_into_memory(rom_file_name)
+
+    def create_gui(self):
+        print("Hi")
+
+    def start_emulator_loop(self):
+        print("hi")
